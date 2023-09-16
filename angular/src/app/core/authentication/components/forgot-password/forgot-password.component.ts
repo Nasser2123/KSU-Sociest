9401bcd06752986daee804d3f0f67339cf7b6c55
SWE. Ahmed Almutairi <a966ab@gmail.com>
2023-09-17 02:32:19 +0300

    forgot-password component preparation

---
+++ angular/src/app/core/authentication/components/forgot-password/forgot-password.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent {
+  forgotPasswordForm: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.forgotPasswordForm.valid) {
+      const email = this.forgotPasswordForm.get('email')!.value;
+      this.authService.forgotPassword(email).subscribe(
+        (response) => {
+          // Reset successful
+          this.successMessage = response.message;
+          this.errorMessage = '';
+        },
+        (error) => {
+          // Handle error
+          this.errorMessage = error.message || 'An error occurred during password reset.';
+          this.successMessage = '';
+        }
+      );
+    }
+  }
+}
