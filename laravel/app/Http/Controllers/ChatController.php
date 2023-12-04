361409293f379278e17d87a339b79033189f15f1
Nasser2123 <nafenasser64a@gmail.com>
2023-12-05 00:21:54 +0300

    Add chat System

---
+++ laravel/app/Http/Controllers/ChatController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\MessageSend;
+use App\Http\Requests\MessageRequest;
+use App\Models\Course;
+use App\Models\Message;
+use App\Traits\HttpResponses;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use function broadcast;
+
+class ChatController extends Controller
+{
+    use HttpResponses;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Course $course
+     * @return JsonResponse
+     */
+    public function index(Course $course):JsonResponse
+    {
+        $messages = $course->message()->orderBy('created_at')->get();
+        return $this->success($messages , "All message belong to this ".$course['name']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Course $course
+     * @param MessageRequest $request
+     * @return JsonResponse
+     */
+    public function store(Course $course ,MessageRequest $request): JsonResponse
+    {
+        $message = Message::create(array_merge(['user_id' => Auth::id() , "course_id" => $course['id']] , $request->all()));
+        broadcast(new MessageSend($request['message']));
+        return $this->success($message, "Message send successfully");
+
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Course $course
+     * @param Message $message
+     * @return JsonResponse
+     */
+    public function destroy(Course $course , Message $message): JsonResponse
+    {
+        if (Auth::id() === $message['user_id']){
+            $message->delete();
+            return $this->success(null, 'We delete the Message from this '. $course['slag']);
+        }
+
+        return $this->error(null, 'We can not delete the Message from this '. $course['slag'] , 401);
+
+    }
+}
---
+++ laravel/app/Models/Message.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @method static create($array_merge)
+ */
+class Message extends Model
+{
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = ['user_id', 'course_id' , 'message'];
+
+
+
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class ,'course_id' , 'id');
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class ,'user_id' , 'id');
+    }
+}
